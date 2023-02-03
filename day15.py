27fadfc76c7ad8ff4fd95e34065e3fad99f316a8
Hyeonseo-Lee <87067373+Hyeonseo-Lee@users.noreply.github.com>
2023-02-03 11:16:11 +0900

    linear list 02

--- day15.py
+++ day15.py
@@ -1,15 +1,23 @@
-pokemons = list()  # 빈 배열
+pokemons = ["피카츄", "라이츄", "꼬부기", "파이리", "이상해"]
 
 
-def add_data(pokemon):
-    pokemons.append(None)
-    #pokemons[len(pokemons) - 1] = pokemon
-    pokemons[-1] = pokemon
+def insert_data(index, pokemon):
+    if index < 0 or index > len(pokemons):
+        print("Out of range!")
+        return
 
-add_data('피카츄')
-add_data('라이츄')
-add_data('파이리')
-add_data('꼬부기')
-add_data('이상해')
+    pokemons.append(None)  # 빈칸 추가
 
+    for i in range(len(pokemons) - 1, index, -1):
+        pokemons[i] = pokemons[i - 1]
+        pokemons[i - 1] = None
+
+    pokemons[index] = pokemon  # 지정한 위치에 친구 추가
+
+if __name__ == "__main__":
 print(pokemons)
+insert_data(2, '거북왕')
+print(pokemons)
+insert_data(6, '어니부기')
+print(pokemons)
+
