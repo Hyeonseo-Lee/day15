1ccc31ec3e3e92169d0ee9a9a625a44b9ff3901f
Hyeonseo-Lee <87067373+Hyeonseo-Lee@users.noreply.github.com>
2023-02-03 14:13:18 +0900

    day15 06

--- day15.py
+++ day15.py
@@ -1,69 +1,48 @@
-pokemons = ["피카츄", "라이츄", "꼬부기", "파이리", "이상해"]
+## 함수 선언 부분 ##
+def print_Poly(px):
+    term = len(px) - 1  # 최고차항 숫자 = 배열길이-1
+    poly_Str = "P(x) = "
 
+    for i in range(len(px)):
+        coef = px[i]  # 계수
 
-def insert_data(index, pokemon):
-    if index < 0 or index > len(pokemons):
-        print("Out of range!")
-        return
+        # if coef >= 0:
+        #     poly_Str = poly_Str + "+"
 
-    pokemons.append(None)  # 빈칸 추가
-
-    for i in range(len(pokemons) - 1, index, -1):
-        pokemons[i] = pokemons[i - 1]
-        pokemons[i - 1] = None
-
-    pokemons[index] = pokemon  # 지정한 위치에 추가
-
-def delete_data(idx):
-    if idx < 0 or idx > len(pokemons):
-        print("Out of range!")
-        return
-
-
-    len_pokemons = len(pokemons)
-    pokemons[idx] = None
-
-    for _ in range(len_pokemons - idx):
-        pokemons.pop()
-
-    #self 3-1
-    # for i in range(idx + 1, len_pokemons):
-    #     pokemons[i] = None
-    #
-    # for i in range(idx, 5):
-    #     pokemons.pop()
-
-def add_data(pokemon):
-
-    pokemons.append(None)
-    pokemons[len(pokemons)-1] = pokemon
-
-pokemons = []
-menu = -1
-
-if __name__ == "__main__":
-
-    while True:
-
-        menu = input("1: 추가, 2: 삽입, 3: 삭제, 4: 종료--> ")
-
-        if (menu == '1'):
-            data = input("추가할 데이터--> ")
-            add_data(data)
-            print(pokemons)
-        elif (menu == '2'):
-            idx = int(input("삽입할 위치--> "))
-            data = input("추가할 데이터--> ")
-            insert_data(idx, data)
-            print(pokemons)
-        elif (menu == '3'):
-            idx = int(input("삭제할 위치--> "))
-            delete_data(idx)
-            print(pokemons)
-        elif (menu == '4'):
-            print(pokemons)
-            exit()
-            #break
-        else:
-            print("menu에서 고르시오")
+        if i > 0 and coef > 0:
+            poly_Str = poly_Str + "+"
+        elif coef == 0:
+            term = term - 1
             continue
+        poly_Str = poly_Str + f'{coef}x^{term}'
+        term = term - 1
+
+    return poly_Str
+
+
+def calc_poly(xVal, p_x):
+    ret_value = 0
+    term = len(p_x) - 1
+
+    for i in range(len(px)):
+        coef = p_x[i]
+        ret_value = ret_value + coef * xVal ** term
+        term -= 1
+
+    return ret_value
+
+
+
+px = [3, -4, 0, 6]
+
+## 메인 코드 부분 ##
+if __name__ == "__main__":
+    pStr = print_Poly(px)
+    print(pStr)
+
+    x_value = int(input("X 값-->"))
+
+    px_value = calc_poly(x_value, px)
+    print(px_value)
+
+
