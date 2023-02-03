a2199398a7e35fa625dc96cb4686ffc6ee101bdb
Hyeonseo-Lee <87067373+Hyeonseo-Lee@users.noreply.github.com>
2023-02-03 11:47:52 +0900

    list 04

--- day15.py
+++ day15.py
@@ -12,12 +12,30 @@ def insert_data(index, pokemon):
         pokemons[i] = pokemons[i - 1]
         pokemons[i - 1] = None
 
-    pokemons[index] = pokemon  # 지정한 위치에 친구 추가
+    pokemons[index] = pokemon  # 지정한 위치에 추가
+
+def delete_data(idx):
+    if idx < 0 or idx > len(pokemons):
+        print("Out of range!")
+        return
+
+
+    len_pokemons = len(pokemons)
+    pokemons[idx] = None
+
+    for i in range(idx + 1, len_pokemons):
+        pokemons[i] = None
+
+    for i in range(idx, 5):
+        pokemons.pop()
+
 
 if __name__ == "__main__":
-print(pokemons)
-insert_data(2, '거북왕')
-print(pokemons)
-insert_data(6, '어니부기')
-print(pokemons)
+
+    print(pokemons)
+    #insert_data(2, '거북왕')
+    delete_data(1)
+    print(pokemons)
+    #insert_data(6, '어니부기')
+    print(pokemons)
 
